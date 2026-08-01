fb1eedaf86379cab5cc386286e059c8f64711714
wmbare25-hub <wmbare25@gmail.com>
2026-08-01 13:29:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const button = document.getElementById("open");
 const letter = document.getElementById("letter");
 
-if (letter) {
-    letter.style.display = "none";
-}
-
 button.addEventListener("click", () => {
+
     letter.style.display = "block";
 
-    button.innerText = "I Love You ❤️";
+    letter.animate(
+        [
+            {opacity:0, transform:"translateY(80px)"},
+            {opacity:1, transform:"translateY(0px)"}
+        ],
+        {
+            duration:1000,
+            fill:"forwards"
+        }
+    );
+
+    button.innerHTML = "❤️ Forever ❤️";
 
     window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: "smooth"
+        top: letter.offsetTop,
+        behavior:"smooth"
     });
+
 });
