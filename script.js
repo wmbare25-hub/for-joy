68e14db8ba76c0a5a31c8c9d04caeacf523022e2
wmbare25-hub <wmbare25@gmail.com>
2026-08-01 13:21:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const button = document.getElementById("open");
+const letter = document.getElementById("letter");
 
+if (letter) {
+    letter.style.display = "none";
+}
+
+button.addEventListener("click", () => {
+    letter.style.display = "block";
+
+    button.innerText = "I Love You ❤️";
+
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    });
+});
